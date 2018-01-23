72d1e48e82b1b260dd5ce0f8d9bfc9e1f2cbe960
jojapoppa <jojapoppa@protonmail.com>
2018-01-22 19:30:55 -0800

    updated email address...

--- contrib/spendfrom/setup.py
+++ contrib/spendfrom/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(name='btcspendfrom',
       version='1.0',
       description='Command-line utility for bitcoin "coin control"',
-      author='Gavin Andresen',
-      author_email='gavin@bitcoinfoundation.org',
+      author='jojapoppa (originally was Gavin Andresen)',
+      author_email='tipsbugreports@protonmail.com',
       requires=['jsonrpc'],
       scripts=['spendfrom.py'],
       )
