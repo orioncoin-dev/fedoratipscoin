dfaedb85b0a2dc1f8a360a12ac6c897acb390664
jojapoppa <jojapoppa@protonmail.com>
2018-02-02 23:17:43 -0800

    test

--- src/qt/snapwidget.cpp
+++ src/qt/snapwidget.cpp
@@ -8,10 +8,10 @@
 #include <QtWidgets/QDesktopWidget>
 #endif
 
-#include <zxing/zxing-cpp-master/core/src/zxing/common/GlobalHistogramBinarizer.h>
-#include <zxing/zxing-cpp-master/core/src/zxing/Binarizer.h>
-#include <zxing/zxing-cpp-master/core/src/zxing/BinaryBitmap.h>
-#include <zxing/zxing-cpp-master/core/src/zxing/MultiFormatReader.h>
+#include <zxing/common/GlobalHistogramBinarizer.h>
+#include <zxing/Binarizer.h>
+#include <zxing/BinaryBitmap.h>
+#include <zxing/MultiFormatReader.h>
 
 #include "qimagesource.h"
 
