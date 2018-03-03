4d8ba4ffabc2e5c51ff5535c1e9133f145742f40
jojapoppa <jojapoppa@protonmail.com>
2018-03-02 16:00:46 -0800

    working on themes...

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -142,6 +142,12 @@ BitcoinGUI::BitcoinGUI(bool fIsTestnet, QWidget *parent) :
         setCentralWidget(rpcConsole);
     }
 
+    // Load external stylesheet
+    QFile File(":styles/fedoracoin.qss");
+    File.open(QFile::ReadOnly);
+    QString StyleSheet = QLatin1String(File.readAll());
+    qApp->setStyleSheet(StyleSheet);
+
     // Accept D&D of URIs
     setAcceptDrops(true);
 
