cb79c3a4194c6a2de1b98326d55a0526060bd14a
jojapoppa <jojapoppa@protonmail.com>
2018-03-06 09:44:04 -0800

    msg color fix

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -47,7 +47,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f, bool isTest
     pixPaint.setPen(QColor(170,170,170));
 
     QFont splashFont;
-    splashFont.setFamily(":fonts/WalletSansFont");
+    splashFont.setFamily(":/fonts/WalletSansFont");
     splashFont.setPointSize(9*fontFactor);
     pixPaint.setFont(splashFont);
     QFontMetrics fm = pixPaint.fontMetrics();
@@ -64,7 +64,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f, bool isTest
     // draw testnet string if testnet is on
     if(isTestNet) {
         QFont boldFont;
-        boldFont.setFamily(":fonts/WalletSansFont");
+        boldFont.setFamily(":/fonts/WalletSansFont");
         boldFont.setPointSize(9*fontFactor);
         boldFont.setWeight(QFont::Bold);
         pixPaint.setFont(boldFont);
@@ -96,7 +96,7 @@ static void InitMessage(SplashScreen *splash, const std::string &message)
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
         Q_ARG(int, Qt::AlignBottom|Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(55,55,55)));
+        Q_ARG(QColor, QColor(155,155,155)));
 }
 
 static void ShowProgress(SplashScreen *splash, const std::string &title, int nProgress)
