1427cff74cf62f29ce20cbe6e312c89a5139b86c
jojapoppa <jojapoppa@protonmail.com>
2018-03-14 15:22:19 -0700

    fixing window background for qrcode..

--- src/qt/snapwidget.cpp
+++ src/qt/snapwidget.cpp
@@ -32,6 +32,8 @@ SnapWidget::SnapWidget(QWidget* _parent) : QDialog(parent, Qt::WindowStaysOnTopH
     setupUi(this);
     this->setMinimumWidth(50);
     prepareMask();
+
+    setStyleSheet("background-color:black;");
     show();
 }
 
