54b92b45c39a4daa663ece83f96e25e339e419c1
jojapoppa <jojapoppa@protonmail.com>
2018-03-17 08:50:28 -0700

    dupe def

--- src/amount.h
+++ src/amount.h
@@ -14,9 +14,8 @@
 /** Amount in satoshis (Can be negative) */
 typedef int64_t CAmount;
 
-static const CAmount COIN = 100000000;
-
 // Removed by Poppa for now
+// static const CAmount COIN = 100000000;
 // static const CAmount CENT = 1000000;
 
 extern const std::string CURRENCY_UNIT;
