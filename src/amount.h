e438a01ee1fee47b47a375033064ec8fcf7c51bd
jojapoppa <jojapoppa@protonmail.com>
2018-03-17 01:30:57 -0700

    formatting bug

--- src/amount.h
+++ src/amount.h
@@ -15,7 +15,9 @@
 typedef int64_t CAmount;
 
 static const CAmount COIN = 100000000;
-static const CAmount CENT = 1000000;
+
+// Removed by Poppa for now
+// static const CAmount CENT = 1000000;
 
 extern const std::string CURRENCY_UNIT;
 
