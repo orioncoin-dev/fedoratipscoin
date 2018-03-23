911bb5cc51fbea1b8f27140d1c3c4ebfdabc26da
jojapoppa <jojapoppa@protonmail.com>
2018-03-23 07:47:25 -0700

    more dark theme work

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -182,6 +182,23 @@ BitcoinGUI::BitcoinGUI(bool fIsTestnet, QWidget *parent) :
         setCentralWidget(rpcConsole);
     }
 
+    // Create a dark pallet
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    qApp->setPalette(darkPalette);
+
     // Load external stylesheet
     QFile File(":themes/fedoracoin.qss");  // remember this can be aliased in the qrc file...
     File.open(QFile::ReadOnly);
