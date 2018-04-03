5e360d3965dc2a54d179f50e778378cb559b00a4
jojapoppa <jojapoppa@protonmail.com>
2018-04-03 16:00:06 -0700

    still working on snap...

--- src/qt/snapwidget.h
+++ src/qt/snapwidget.h
@@ -8,23 +8,19 @@
 #include <QtMultimedia/QtMultimedia>
 #include <QtMultimediaWidgets/qcameraviewfinder.h>
 
-#include <QtMultimedia/QCameraImageCapture>
-#include <QtMultimedia/QMediaRecorder>
-#include <QtCore/QScopedPointer>
-
-namespace Ui { class SnapWidget; }
-
 class SnapWidget :public QDialog, public Ui::SnapWidget
 {
     Q_OBJECT
+    QCamera *camera;
 public:
 
     SnapWidget(QWidget* _parent);
     ~SnapWidget();
-    // virtual void closeEvent(QCloseEvent *event);
+    virtual void closeEvent(QCloseEvent *event);
     public slots:
     void on_cancelButton_clicked();
     void on_snapButton_clicked();
+
 
 signals:
     void finished(QString s);
@@ -32,64 +28,5 @@ signals:
 public:
     QString decodedString;
 
-private slots:
-    void setCamera(const QCameraInfo &cameraInfo);
-
-    void startCamera();
-    void stopCamera();
-
-    void record();
-    void pause();
-    void stop();
-    void setMuted(bool);
-
-    void toggleLock();
-    void takeImage();
-    void displayCaptureError(int, QCameraImageCapture::Error, const QString &errorString);
-
-    void configureCaptureSettings();
-    void configureVideoSettings();
-    void configureImageSettings();
-
-    void displayRecorderError();
-    void displayCameraError();
-
-    void updateCameraDevice(QAction *action);
-
-    void updateCameraState(QCamera::State);
-    void updateCaptureMode();
-    void updateRecorderState(QMediaRecorder::State state);
-    void setExposureCompensation(int index);
-
-    void updateRecordTime();
-
-    void processCapturedImage(int requestId, const QImage &img);
-    void updateLockStatus(QCamera::LockStatus, QCamera::LockChangeReason);
-
-    void displayViewfinder();
-    void displayCapturedImage();
-
-    void readyForCapture(bool ready);
-    void imageSaved(int id, const QString &fileName);
-
-protected:
-    void keyPressEvent(QKeyEvent *event) override;
-    void keyReleaseEvent(QKeyEvent *event) override;
-    void closeEvent(QCloseEvent *event) override;
-
-private:
-    Ui::SnapWidget *ui;
-
-    QScopedPointer<QCamera> m_camera;
-    QScopedPointer<QCameraImageCapture> m_imageCapture;
-    QScopedPointer<QMediaRecorder> m_mediaRecorder;
-
-    QImageEncoderSettings m_imageSettings;
-    QAudioEncoderSettings m_audioSettings;
-    QVideoEncoderSettings m_videoSettings;
-    QString m_videoContainerFormat;
-    bool m_isCapturingImage = false;
-    bool m_applicationExiting = false;
 };
-
 #endif // SNAPWIDGET_H
