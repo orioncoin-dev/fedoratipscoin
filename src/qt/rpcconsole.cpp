f32a9b324a23d225412ff6b4a94b5410f575b32d
jojapoppa <jojapoppa@protonmail.com>
2018-04-09 20:10:54 -0700

    exception...

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -170,10 +170,6 @@ void RPCExecutor::request(const QString &command)
 
         emit reply(RPCConsole::CMD_REPLY, QString::fromStdString(strPrint));
     }
-    catch (JSONRPCError &e)
-    {
-        emit reply(RPCConsole::CMD_ERROR, "unrecognized command: "+command);
-    }
     catch (json_spirit::Object& objError)
     {
         try // Nice formatting for standard-format error
