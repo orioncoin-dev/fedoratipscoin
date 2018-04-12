c9c9f5f748d9c2c3d1aaf6ad6745a73a73935f53
jojapoppa <jojapoppa@protonmail.com>
2018-04-12 12:32:05 -0700

    working on command line error on exit...

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -1028,7 +1028,7 @@ void ShutdownWindow::showShutdownWindow(BitcoinGUI *window)
         return;
 
     // Show a simple window indicating shutdown status
-    QWidget *shutdownWindow = new QWidget(window);
+    QWidget *shutdownWindow = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget(new QLabel(
         tr("Fedoracoin Core is shutting down...") + "<br /><br />" +
