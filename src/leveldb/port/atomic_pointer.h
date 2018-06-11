76651b311fb05f0fae511aa19985c71ddc0c4c33
Jo Poppa <jojapoppa@protonmail.com>
2018-06-11 03:00:15 +0000

    removed std::

--- src/leveldb/port/atomic_pointer.h
+++ src/leveldb/port/atomic_pointer.h
@@ -55,7 +55,7 @@ namespace port {
 #elif defined(OS_MACOSX)
 inline void MemoryBarrier() {
   //OSMemoryBarrier();
-  std::atomic_thread_fence(std::memory_order_seq_cst);
+  atomic_thread_fence(memory_order_seq_cst);
 }
 #define LEVELDB_HAVE_MEMORY_BARRIER
 
